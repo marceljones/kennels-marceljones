238eb5db233f31620a86c2631efe2d80b1261ff1
marceljones <marceljones.mj@gmail.com>
2020-10-02 13:54:27 -0500

    initial commit

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,13 +1,55 @@
 import React from "react"
-import "./Kennel.css"
+import { AnimalCard } from "./animal/AnimalCard"
+import { CustomerCard } from "./customers/CustomerCard"
+import { EmployeeCard } from "./employees/EmployeeCard";
+import { LocationCard } from "./locations/LocationCard";
+import "./animal/Animal.css"
+import "./customers/Customers.css"
+import "./employees/Employees.css"
+import "./locations/Locations.css"
+
+
+
 
 export const Kennel = () => (
     <>
         <h2>Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
+
         <address>
             <div>Visit Us at the Nashville North Location</div>
             <div>500 Puppy Way</div>
         </address>
+
+        <h2>Animals</h2>
+        <article className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+             
+        </article>
+        <h2>Customers</h2>
+        <article className="customers">
+            <CustomerCard />
+            <CustomerCard />
+            <CustomerCard />
+             
+        </article>
+        <h2>Employees</h2>
+        <article className="employees">
+            <EmployeeCard />
+            <EmployeeCard />
+            <EmployeeCard />
+             
+        </article>
+        {/* <h2>Locations</h2>
+        <article className="locations">
+            <LocationCard />
+            <LocationCard />
+            <LocationCard />
+             
+        </article> */}
     </>
 )
+
+
---
+++ src/components/locations/LocationCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Locations.css"
+
+export const LocationsCard = () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels North</h3>
+        <div className="location__address">Address: 200 Main Street</div>
+    </section>
+)
